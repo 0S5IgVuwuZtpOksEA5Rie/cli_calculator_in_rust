5f0b1e1840a41864e80d59f12fe9055bd80704e9
0S5IgVuwuZtpOksEA5Rie <3QZWZtfVCyVX6v@gmail.com>
2022-11-09 07:26:47 +1000

    Made functions for basic mathematics arithmatic.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,19 @@
+fn subtraction(int term1, int term2) {
+    
+}
+
+fn addition(int term1, int term2) {
+    
+}
+
+fn multiplication(int term1, int term2) {
+    
+}
+
+fn division(int term1, int term2) {
+    
+}
+
 fn main() {
     println!("Hello, world!");
 }
